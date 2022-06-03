aa5d8d238705651ecfd4230968cbbca7e2390c03
bhatiagagan24 <bhatiagagan24@gmail.com>
2022-06-03 22:18:13 +0530

    uid

--- src/uid_gen.py
+++ src/uid_gen.py
@@ -1,14 +1,11 @@
 import uuid
 class Userid:
-
     def __init__(self):
-        self.uid=0
-        
-
+        pass
     def uid_gen(self):
-        
         self.uid=uuid.uuid4()
         return self.uid
+
 u=Userid()
 id=u.uid_gen()
 print(id)
