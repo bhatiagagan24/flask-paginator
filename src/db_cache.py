4c261319a4afdb675e58893f5499b94b4b2cc401
Himanshu2708 <63806659+Himanshu2708@users.noreply.github.com>
2022-06-03 22:29:38 +0530

    added commands

--- src/db_cache.py
+++ src/db_cache.py
@@ -7,8 +7,15 @@ class Db_Ops:
         self.con = sq.connect(database_name + ".db")
         self.con.execute(command)
     def insert_into_db(self, uid: str, start_range : int, end_range: int):
-        self.con.execute('''INSERT INTO ''')        
+        self.con.execute("INSERT INTO table_paginator values("+"'"+uid+"',"+start+","+end+")")        
     def remove_from_db(self, uid : str):
-        pass
+        cmd="DELETE from table_paginator where uid="+"'"+uid+"'"
+        cur=self.conn.cursor()
+        cur.execute(cmd)
+        self.con.commit()
     def fetch_from_db(self, uid : str):
-        pass
+        cmd="SELECT * from table_paginator where uid="+"'"+uid+"'"
+        cur=self.conn.cursor()
+        cur.execute(cmd)
+        result = cur.fetchall()
+        return result
